0c817e878c4955552a87561a8bf31d3c8b56ccc5
Phong Lo <phonglo724@gmail.com>
2021-02-01 01:23:19 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,15 +1,15 @@
-# birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
-#   "Amanda" => 27
-# }
+birthday_kids = {
+  "Timmy" => 9, 
+  "Sarah" => 6, 
+  "Amanda" => 27
+}
 
-# def happy_birthday(birthday_kids)
-# birthday_kids.each do |name, age|
-#   puts "Happy Birthday #{name}! You are now #{age} years old!"
-# end
-# birthday_kids
-# end
+def happy_birthday(birthday_kids)
+birthday_kids.each do |name, age|
+  puts "Happy Birthday #{name}! You are now #{age} years old!"
+end
+birthday_kids
+end
 
 def happy_birthday(kids)
   kids.each do |key, value|
